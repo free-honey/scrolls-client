73ca37977dd4f83ce9e6636ca4a93d5592ca0015
Turner <james.mitchell.turner@gmail.com>
2023-09-02 21:29:19 -0700

    Improve output example

--- examples/full_utxos.rs
+++ examples/full_utxos.rs
@@ -1,6 +1,5 @@
 use pallas_addresses::Address;
 use redis;
-use redis::Commands;
 use scrolls_client::ScrollsClient;
 use scrolls_client::UTxOsByAddress;
 
@@ -10,7 +9,8 @@ async fn main() {
     let port = "6379".to_string();
     let client = ScrollsClient::new_redis(ip, port);
 
-    let address_str = "addr_test1qp7dqz7g6nyg0y08np42aj8magcwdgr8ea6mysa7e9f6qg8hdg3rkwaqkqysqnwqsfl2spx4yreqywa6t5mgftv6x3fsckw6qg";
+    // let address_str = "addr_test1qp7dqz7g6nyg0y08np42aj8magcwdgr8ea6mysa7e9f6qg8hdg3rkwaqkqysqnwqsfl2spx4yreqywa6t5mgftv6x3fsckw6qg";
+    let address_str = "addr_test1wq6t9y9k20wp545s2snkt5222vhhwt40p8mqt8pad6xtdnsq95tm0";
     let address = Address::from_bech32(address_str).unwrap();
     let outputs = client.get_utxos_for_address(&address).await.unwrap();
 
@@ -18,12 +18,26 @@ async fn main() {
     println!();
     for utxo in outputs {
         println!(
-            "index: {:?}, tx_hash: {:?}",
+            "index: {:?}, tx_hash: {:?}, datum: {:?}",
             utxo.output_index(),
-            utxo.tx_hash()
+            utxo.tx_hash(),
+            utxo.datum(),
         );
         for amount in utxo.amount() {
-            println!("{:?}", amount);
+            let quantity = amount.quantity();
+            let unit = amount.unit().to_string();
+            if unit == "lovelace" {
+                println!("quantity: {:?}, unit: {:?}", quantity, unit);
+            } else {
+                let policy_id = &unit[..56];
+                let token_name = &unit[56..];
+                let token_name_str: String =
+                    String::from_utf8(hex::decode(token_name).unwrap()).unwrap();
+                println!(
+                    "quantity: {:?}, policy_id: {:?}, token_name: {:?}",
+                    quantity, policy_id, token_name_str
+                );
+            }
         }
         println!();
     }
